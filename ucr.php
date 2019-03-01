5506db1fa57f2a4ee0454fc732fd2717e2a4950d
GAMMZ <37985813+GAMMZ@users.noreply.github.com>
2019-03-01 20:26:59 +0700

    Create ucr.php

---
+++ ucr.php
@@ -0,0 +1,21 @@
+
+### <?php
+echo $red."[!] warning !!!".$t;
+echo $kuning."[-] ini adalah program ilegal".$t;
+echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+$stat_ads=true;
+$ads= $putih.'[*] '.$ijo.'subcribe youtube sungging '.$t.$putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]".$t; 
+$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$ijo."[•••••] sekilas info [••••••]";
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+//$weblock =["Imam Almasyah Risanzani",];
+//$ydblock = ["Imam Almasyah Risanzani",];
+$we_stat = true;
+$msgwe = "[•] update member ok ".$t.$kuning."[•] untuk menjadi member gunakan kode invite di atas👆👆".$t;
+$donasi=true;
+$msgveeu = "[!] update skrip veeu".$t."[•] git clone https://github.com/adidoank/veeu";
+$data= ['GAMMZ', 'Muhammad Aminuddin',
+       ];
+$toto = ['tes',];
+$ydnews = ['tes','adidoank',];
